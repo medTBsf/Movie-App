3e18ca7b0b38ea3d9bd55573a5c2a0dc7437a5fa
Medinho89 <mohamedtaiebbsf@yahoo.com>
2018-09-01 19:13:32 +0200

    create Rating component and insert it into MovieCard component

---
+++ src/components/rating/Rating.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Rating = () => {
+  return (
+    <div className="rating">
+      <i className="fa fa-star" hidden-area="true" />
+      <i className="fa fa-star" hidden-area="true" />
+      <i className="fa fa-star" hidden-area="true" />
+      <i className="fa fa-star" hidden-area="true" />
+      <i className="fa fa-star-o" hidden-area="true" />
+    </div>
+  );
+};
+
+export default Rating;
